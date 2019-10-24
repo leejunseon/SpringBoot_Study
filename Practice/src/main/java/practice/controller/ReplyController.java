336cdf980de361edd7b7a7bc1ce57e2ac23f7f07
이준선 <ljs921026@gmail.com>
2019-10-24 21:05:37 +0900

    2019-10-24
    댓글 crud 구현 및 테스트 작성

--- Practice/src/main/java/practice/controller/ReplyController.java
+++ Practice/src/main/java/practice/controller/ReplyController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -34,5 +36,20 @@ public class ReplyController {
 	public ResponseEntity<List<ReplyVO>> getReplies(@PathVariable("page")int page,@PathVariable("bno")Long bno) throws Exception{
 		ReplyPagingDto paging=new ReplyPagingDto(page,10);
 		return new ResponseEntity<>(service.getReplies(bno, paging),HttpStatus.OK);
+	}
+	
+	@GetMapping(value="/{rno}",produces= {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno")Long rno) throws Exception{
+		return new ResponseEntity<>(service.get(rno),HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value="/{rno}",produces= {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno")Long rno) throws Exception{
+		return service.remove(rno)==1?new ResponseEntity<>("success",HttpStatus.OK):new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@RequestMapping(method= {RequestMethod.PUT,RequestMethod.PATCH},value="/{rno}",consumes="application/json",produces= {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String>modify(@RequestBody ReplyVO vo,@PathVariable("rno")Long rno) throws Exception{
+		return service.modify(vo)==1?new ResponseEntity<>("success",HttpStatus.OK):new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
--- Practice/src/test/java/practice/controller/ReplyControllerTests.java
+++ Practice/src/test/java/practice/controller/ReplyControllerTests.java
@@ -1,7 +1,9 @@
 package practice.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -46,6 +48,35 @@ public class ReplyControllerTests {
 	/*@Test
 	public void testGetReplies() throws Exception{
 		log.info(mockMvc.perform(get("/replies/pages/20/1"))
+				.andReturn()
+				.getResponse()
+				.getContentAsString()
+				);
+	}*/
+	
+	/*@Test
+	public void testGet() throws Exception{
+		log.info(mockMvc.perform(get("/replies/2"))
+				.andReturn()
+				.getResponse()
+				.getContentAsString()
+				);
+	}*/
+	
+	/*@Test
+	public void testDelete() throws Exception{
+		log.info(mockMvc.perform(delete("/replies/2"))
+				.andReturn()
+				.getResponse()
+				.getContentAsString()
+				);
+	}*/
+	
+	/*@Test
+	public void testRegister() throws Exception{
+		log.info(mockMvc.perform(put("/replies/30")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"rno\":\"30\",\"bno\":\"20\",\"reply\":\"수정수정\",\"replyer\":\"user\"}"))
 				.andReturn()
 				.getResponse()
 				.getContentAsString()
