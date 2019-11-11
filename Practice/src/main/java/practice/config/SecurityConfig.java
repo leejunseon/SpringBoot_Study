dc34434cc6bf81b20ec703d6f55f0b8339b76585
이준선 <ljs921026@gmail.com>
2019-11-11 22:02:44 +0900

    2019-11-11
    list.html csrf토큰 사용

--- Practice/src/main/java/practice/config/SecurityConfig.java
+++ Practice/src/main/java/practice/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package practice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception{
+		log.info("Security Configure");
+		http.authorizeRequests()
+			.antMatchers("/board/list").permitAll();
 		
+		http.formLogin().loginPage("/customLogin").loginProcessingUrl("/login");
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		log.info("Security Configure");
 	}
 }
