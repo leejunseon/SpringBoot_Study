6dfa0a2d2a34a7dfb1c6fccaa8fdd0e0d21475c9
이준선 <ljs921026@gmail.com>
2019-11-19 22:16:43 +0900

    2019-11-19

--- Practice/src/main/java/practice/controller/MemberController.java
+++ Practice/src/main/java/practice/controller/MemberController.java
@@ -1,11 +1,19 @@
 package practice.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import practice.domain.MemberVO;
 import practice.service.MemberService;
 
@@ -13,6 +21,7 @@ import practice.service.MemberService;
 @Controller
 @RequestMapping("/member")
 @AllArgsConstructor
+@Slf4j
 public class MemberController {
 	
 	private MemberService service;
@@ -23,8 +32,17 @@ public class MemberController {
 	}
 	
 	@PostMapping("/register")
-	public String register(MemberVO vo) {
-		service.insertUser(vo);
-		return "redirect:/customLogin";
+	public String register(@Valid MemberVO member,BindingResult result) {
+		if(result.hasErrors()) {
+			log.info("Validation Error");
+			List<ObjectError> list=result.getAllErrors();
+			for(ObjectError error:list) {
+				log.info(error.toString());
+			}
+			return "/member/register";
+		}else {
+			service.insertUser(member);
+			return "redirect:/customLogin";
+		}
 	}
 }
--- Practice/src/main/java/practice/domain/MemberVO.java
+++ Practice/src/main/java/practice/domain/MemberVO.java
@@ -3,17 +3,26 @@ package practice.domain;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class MemberVO {
 	
+	@NotBlank(message = "Id is mandatory")
 	private String userid;
+	
+	@NotBlank(message = "Password is mandatory")
 	private String userpw;
+	
+	@NotBlank(message = "Name is mandatory")
 	private String userName;
-	private boolean enabled;
+	
+	@NotBlank(message = "Email is mandatory")
 	private String email;
 	
+	private boolean enabled;
 	private Date regDate;
 	private Date updateDate;
 	private List<AuthVO> authList;
