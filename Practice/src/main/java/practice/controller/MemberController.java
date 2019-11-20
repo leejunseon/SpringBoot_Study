1a6aa886ef586dc7e2184279857ffa6fb8556cfc
이준선 <ljs921026@gmail.com>
2019-11-20 23:17:03 +0900

    2019-11-20

--- Practice/src/main/java/practice/controller/MemberController.java
+++ Practice/src/main/java/practice/controller/MemberController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,7 +26,7 @@ public class MemberController {
 	private MemberService service;
 	
 	@GetMapping("/register")
-	public void register() {
+	public void register(MemberVO member) {
 		
 	}
 	
