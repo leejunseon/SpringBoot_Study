69abe6fb6570dc8e5c5a7bd62222e9839e91351e
leejunseon <ljs921026@gmail.com>
2019-12-10 19:51:48 +0900

    no message

---
+++ Practice/src/main/java/practice/security/SecurityInitializer.java
@@ -0,0 +1,8 @@
+package practice.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//시큐리티 필터를 추가하는 클래스
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
